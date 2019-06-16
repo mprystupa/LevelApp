d4164d2488317b38c408199984e43ac38cefccfe
Yoshua <marcinp32@gmail.com>
2019-06-16 21:35:52 +0200

    Change Vue framework to Quasar

---
+++ LevelApp.Client/src/pages/Index.vue
@@ -0,0 +1,14 @@
+<template>
+  <q-page class="flex flex-center">
+    <img alt="LevelApp logo" width="500px" src="~assets/levelapp-logo-full.svg">
+  </q-page>
+</template>
+
+<style>
+</style>
+
+<script>
+export default {
+  name: 'PageIndex'
+}
+</script>
