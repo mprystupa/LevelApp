ecf63ad9e58b4c42bb4552c740473ee6cf95d20d
Yoshua <marcinp32@gmail.com>
2019-09-28 19:50:36 +0200

    SonarQube code coverage setup

--- LevelApp.Utils/RunSonarScanner.bat
+++ LevelApp.Utils/RunSonarScanner.bat
@@ -1,7 +1,7 @@
+cd ..
 dotnet test LevelApp.BLL.Tests\LevelApp.BLL.Tests.csproj /p:CollectCoverage=true /p:CoverletOutputFormat=opencover
 dotnet test LevelApp.DAL.Tests\LevelApp.DAL.Tests.csproj /p:CollectCoverage=true /p:CoverletOutputFormat=opencover
 dotnet sonarscanner begin /k:"levelapp" /d:sonar.host.url="http://localhost:9000" /d:sonar.login="c57a119269d225338818b4b2b262c88f5012e9fd"
-cd ..
 dotnet build LevelApp.sln
 dotnet sonarscanner end /d:sonar.login="c57a119269d225338818b4b2b262c88f5012e9fd"
 
