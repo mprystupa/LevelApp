e0b8718fc24779a3885cf3e8f7336a01aa76e5d2
Yoshua <marcinp32@gmail.com>
2020-01-07 16:15:29 +0100

    Add starting lesson implementation

--- LevelApp.Client/src/components/main/courses/CourseTreeEditor.vue
+++ LevelApp.Client/src/components/main/courses/CourseTreeEditor.vue
@@ -11,7 +11,20 @@
     <q-popup-proxy context-menu>
       <div v-if="isContextMenuVisible">
         <q-btn-group push>
-          <q-btn push label="Remove" icon="fas fa-times" @click="onRemoveClick" />
+          <q-btn
+            push
+            v-if="contextMenuVisibility.remove"
+            label="Remove"
+            icon="fas fa-times"
+            @click="onRemoveClick"
+          />
+          <q-btn
+            push
+            label="Set as starting lesson"
+            v-if="contextMenuVisibility.setAsStarting"
+            icon="fas fa-home"
+            @click="onSetAsStartingClick"
+          />
         </q-btn-group>
       </div>
     </q-popup-proxy>
@@ -30,6 +43,7 @@ Cytoscape.use(Popper);
 
 export default {
   cytoscape: {},
+  edgehandles: {},
   selectedNode: null,
   name: "CourseTreeEditor",
   props: ["value", "cytoscapeOptions", "readOnly"],
@@ -37,156 +51,202 @@ export default {
     return {
       isNodeSelected: !!this.selectedNode,
       isContextMenuVisible: false,
-      treeValue: {}
+      contextMenuVisibility: {
+        remove: true,
+        setAsStarting: false
+      },
+      treeValue: {},
+      lessonNodeSelector: 'node[name]'
     };
   },
   mounted() {
-    let options = {
-      container: this.$refs.cytoscape,
-      style: [
-        // the stylesheet for the graph
-        {
-          selector: "node",
-          style: {
-            "background-color": "#666",
-            height: "90",
-            width: "90",
-            shape: "polygon",
-            "shape-polygon-points":
-              "0, 1, 0.866, 0.5, 0.866, -0.5, 0, -1, -0.866, -0.5, -0.866, 0.5",
-            "border-width": "4",
-            "border-color": "#fff",
-            label: "data(name)"
-          }
-        },
-        {
-          selector: "node:selected",
-          style: {
-            "border-color": "#0fb"
-          }
-        },
-        {
-          selector: ".label-background",
-          style: {
-            "font-family": "'Istok Web', sans-serif",
-            "text-background-padding": 4,
-            "text-background-opacity": 1,
-            color: "#000",
-            "text-background-color": "#fff",
-            "text-background-shape": "roundrectangle"
-          }
-        },
-        {
-          selector: ".label-bottom",
-          style: {
-            "text-valign": "bottom",
-            "text-halign": "center"
-          }
-        },
-        {
-          selector: "edge",
-          style: {
-            width: 4,
-            "line-color": "#fff",
-            "line-style": "dashed",
-            "target-arrow-color": "#ccc",
-            "target-arrow-shape": "triangle"
-          }
-        },
-        {
-          selector: ".eh-handle",
-          style: {
-            "background-color": "red",
-            width: 12,
-            height: 12,
-            shape: "ellipse",
-            "overlay-opacity": 0,
-            "border-width": 12, // makes the handle easier to hit
-            "border-opacity": 0,
-            label: ""
-          }
-        },
-        {
-          selector: ".eh-hover",
-          style: {
-            "background-color": "red"
-          }
-        },
-        {
-          selector: ".eh-source",
-          style: {
-            "border-width": 2,
-            "border-color": "red"
-          }
-        },
-        {
-          selector: ".eh-target",
-          style: {
-            "border-width": 2,
-            "border-color": "red"
-          }
-        },
-        {
-          selector: ".eh-preview, .eh-ghost-edge",
-          style: {
-            "background-color": "red",
-            "line-color": "red",
-            "target-arrow-color": "red",
-            "source-arrow-color": "red"
-          }
-        },
-        {
-          selector: ".eh-ghost-edge.eh-preview-active",
-          style: {
-            opacity: 0
-          }
-        }
-      ],
-      autoungrabify: true
-    };
-
-    if (this.cytoscapeOptions) {
-      options = this.cytoscapeOptions;
-    }
-
-    let cy = Cytoscape(options);
-
-    cy.gridGuide({
-      snapToGridDuringDrag: true,
-      drawGrid: true,
-      panGrid: true,
-      distributionGuidelines: true,
-      gridSpacing: 40,
-      guidelinesStyle: {
-        strokeStyle: "black",
-        horizontalDistColor: "#ff0000",
-        verticalDistColor: "green",
-        initPosAlignmentColor: "#0000ff"
-      }
-    });
-
-    if (!this.readOnly) {
-      this.initEditMode(cy);
-    }
-
-    cy.minZoom(0.5);
-    cy.maxZoom(2);
-
-    if (this.value) {
-      cy.json(this.value);
-      this.treeValue = this.value;
-    }
-
-    this.registerDataChangeEvents(["add", "move", "remove", "free"], cy);
-    this.registerElementSelectionEvents(cy);
-
-    this.cytoscape = cy;
+    this.initCytoscape();
   },
   methods: {
+    /**
+     * Initializes Cytoscape library with options
+     */
+    initCytoscape() {
+      let options = {
+        container: this.$refs.cytoscape,
+        style: [
+          // the stylesheet for the graph
+          {
+            selector: this.lessonNodeSelector,
+            style: {
+              "background-color": "#666",
+              height: "90",
+              width: "90",
+              shape: "polygon",
+              "shape-polygon-points":
+                "0, 1, 0.866, 0.5, 0.866, -0.5, 0, -1, -0.866, -0.5, -0.866, 0.5",
+              "border-width": "4",
+              "border-color": "#fff",
+              label: "data(name)"
+            }
+          },
+          {
+            selector: "node:selected",
+            style: {
+              "border-color": "#0fb"
+            }
+          },
+          {
+            selector: "node:unselected.first-lesson",
+            style: {
+              "border-color": "#f2ff23"
+            }
+          },
+          {
+            selector: ".label-background",
+            style: {
+              "font-family": "'Istok Web', sans-serif",
+              "text-background-padding": 4,
+              "text-background-opacity": 1,
+              color: "#000",
+              "text-background-color": "#fff",
+              "text-background-shape": "roundrectangle"
+            }
+          },
+          {
+            selector: ".label-bottom",
+            style: {
+              "text-valign": "bottom",
+              "text-halign": "center"
+            }
+          },
+          {
+            selector: "edge",
+            style: {
+              width: 4,
+              "line-color": "#fff",
+              "line-style": "dashed",
+              "target-arrow-color": "#ccc",
+              "target-arrow-shape": "triangle"
+            }
+          },
+          {
+            selector: ".eh-handle",
+            style: {
+              "background-color": "red",
+              width: 12,
+              height: 12,
+              shape: "ellipse",
+              "overlay-opacity": 0,
+              "border-width": 12, // makes the handle easier to hit
+              "border-opacity": 0,
+              label: ""
+            }
+          },
+          {
+            selector: ".eh-hover",
+            style: {
+              "background-color": "red"
+            }
+          },
+          {
+            selector: ".eh-source",
+            style: {
+              "border-width": 2,
+              "border-color": "red"
+            }
+          },
+          {
+            selector: ".eh-target",
+            style: {
+              "border-width": 2,
+              "border-color": "red"
+            }
+          },
+          {
+            selector: ".eh-preview, .eh-ghost-edge",
+            style: {
+              "background-color": "red",
+              "line-color": "red",
+              "target-arrow-color": "red",
+              "source-arrow-color": "red"
+            }
+          },
+          {
+            selector: ".eh-ghost-edge.eh-preview-active",
+            style: {
+              opacity: 0
+            }
+          }
+        ],
+        autoungrabify: true
+      };
+
+      if (this.cytoscapeOptions) {
+        options = this.cytoscapeOptions;
+      }
+
+      let cy = Cytoscape(options);
+
+      cy.gridGuide({
+        snapToGridDuringDrag: true,
+        drawGrid: true,
+        panGrid: true,
+        distributionGuidelines: true,
+        gridSpacing: 40,
+        guidelinesStyle: {
+          strokeStyle: "black",
+          horizontalDistColor: "#ff0000",
+          verticalDistColor: "green",
+          initPosAlignmentColor: "#0000ff"
+        }
+      });
+
+      if (!this.readOnly) {
+        this.initEditMode(cy);
+      }
+
+      cy.minZoom(0.5);
+      cy.maxZoom(2);
+
+      if (this.value) {
+        cy.json(this.value);
+        this.treeValue = this.value;
+      }
+
+      this.registerEventHandlers(cy);
+
+      this.cytoscape = cy;
+    },
+
+    /**
+     * Initializes Cytoscape tree edit mode
+     * @param cy: Cytoscape Core object
+     */
     initEditMode(cy) {
       cy.autoungrabify(false);
-      cy.edgehandles();
+      this.edgehandles = cy.edgehandles({
+        // Completed edge handler
+        complete: (sourceNode, targetNode, addedEles) => {
+          try {
+            this.validateEdge(sourceNode, targetNode, addedEles);
+          } catch (error) {
+            this.cytoscape.remove(addedEles);
+            console.error(error);
+          }
+        }
+      });
     },
+
+    /**
+     * Registers all handlers for events appearing on Cytoscape editor
+     * @param cy: Cytoscape Core object
+     */
+    registerEventHandlers(cy) {
+      this.registerDataChangeEvents(["add", "move", "remove", "free"], cy);
+      this.registerElementSelectionEvents(cy);
+    },
+
+    /**
+     * Registers all event handlers that are executed by any data change event
+     * @param eventList: list of events causing data change (passed as strings array)
+     * @param cy: Cytoscape Core object
+     */
     registerDataChangeEvents(eventList, cy) {
       eventList.forEach(event => {
         cy.on(event, () => {
@@ -194,44 +254,42 @@ export default {
         });
       });
     },
+
+    /**
+     * Registers all event handlers that are executed by user input
+     * @param cy: Cytoscape Core object
+     */
     registerElementSelectionEvents(cy) {
       // Right click context menu implementation
       cy.on("cxttap", event => {
-        let target = event.target;
-
-        // Deselect currently selected node
-        if (this.selectedNode) {
-          this.selectedNode.unselect();
-        }
-
-        // Select new node
-        if (target && target.data() && Object.keys(target.data()).length) {
-          target.select();
-          this.isContextMenuVisible = true;
-        }
+        this.onContextTap(event);
       });
 
       cy.on("select", event => {
-        if (this.selectedNode) {
-          this.isContextMenuVisible = false;
-        }
-
-        this.selectedNode = event.target;
+        this.onElementSelect(event);
       });
 
       cy.on("unselect", event => {
-        this.selectedNode = null;
-        this.isContextMenuVisible = false;
+        this.onElementUnselect();
       });
     },
+
+    /**
+     * Adds new lesson node to existing tree; throws error when executed in read-only mode
+     * @param lessonData: data of added lesson
+     * @param position: position of added lesson (in window coordinates)
+     */
     addLesson(lessonData, position) {
       if (!this.readOnly) {
+        let isFirstLesson = this.cytoscape.nodes().empty();
+
         this.cytoscape.add({
           data: {
             id: lessonData.id,
-            name: lessonData.name
+            name: lessonData.name,
+            isFirst: isFirstLesson
           },
-          classes: "label-background label-bottom",
+          classes: `label-background label-bottom ${isFirstLesson ? 'first-lesson' : ''}`,
           renderedPosition: {
             x: position.x,
             y: position.y
@@ -241,23 +299,163 @@ export default {
         throw new Error("Cannot add lessons in read-only mode!");
       }
     },
+
+    /**
+     * Removes tree element; if lesson node is deleted, proper event is emitted as well
+     * @param element: element to remove from tree
+     */
     removeElement(element) {
       this.cytoscape.remove(element);
 
       // Emit lesson deleted event
       if (element.isNode()) {
-        console.log(element);
-        this.$emit('removeLesson', element.data("id"));
+        this.$emit("removeLesson", element.data("id"));
       }
     },
+
+    /**
+     * Validates if edge is not breaking any tree rules
+     */
+    validateEdge(sourceNode, targetNode, addedEdge) {
+      // No parallel edges
+      if (this.detectParallelEdge(targetNode)) {
+        throw new Error("Parallel edges are not allowed");
+      }
+
+      // No loops
+      if (this.detectLoop()) {
+        throw new Error("Loops are not allowed");
+      }
+    },
+
+    /**
+     * Detects parallel edge on element
+     */
+    detectParallelEdge(element) {
+      let elementEdges = element.connectedEdges();
+      return elementEdges.toArray().find(x => x.parallelEdges().length > 1);
+    },
+
+    /**
+     * Detects loop in whole tree
+     */
+    detectLoop() {
+      let allNodes = this.cytoscape.nodes(this.lessonNodeSelector).toArray().filter(x => x.isNode());
+      let visitedNodeIds = [];
+      let nodesToVisit = [];
+
+      if (allNodes.length > 0) {
+        nodesToVisit.push(allNodes[0]);
+      }
+
+      while (nodesToVisit.length > 0) {
+        let currentNode = nodesToVisit.pop();
+
+        if (visitedNodeIds.includes(currentNode.data('id'))) {
+          return true;
+        }
+
+        visitedNodeIds.push(currentNode.data('id'));
+
+        currentNode.outgoers(this.lessonNodeSelector).forEach(node => {
+          if (!visitedNodeIds.includes(node.data('id'))) {
+            nodesToVisit.push(node);
+          }
+        });
+
+        currentNode.incomers(this.lessonNodeSelector).forEach(node => {
+          if (!visitedNodeIds.includes(node.data('id'))) {
+            nodesToVisit.push(node);
+          }
+        });
+      }
+
+      return false;
+    },
+
+    /**
+     * Handles context tap (right click) event
+     * @param event: event data
+     */
+    onContextTap(event) {
+      let target = event.target;
+
+      // Deselect currently selected node
+      if (this.selectedNode) {
+        this.selectedNode.unselect();
+      }
+
+      // Select new node and recalculate options visibility
+      if (target && target.data() && Object.keys(target.data()).length) {
+        target.select();
+        this.setContextMenuVisibility();
+        this.isContextMenuVisible = true;
+      }
+    },
+
+    /**
+     * Handles element selection event
+     * @param event: event data
+     */
+    onElementSelect(event) {
+      if (this.selectedNode) {
+        this.isContextMenuVisible = false;
+      }
+
+      this.selectedNode = event.target;
+    },
+
+    /**
+     * Handles element deselection event
+     */
+    onElementUnselect() {
+      this.selectedNode = null;
+      this.isContextMenuVisible = false;
+    },
+
+    /**
+     * Handles tree data change event
+     */
     onDataChange() {
       this.$emit("input", this.cytoscape.json());
     },
+
+    /**
+     * Handles remove element button click
+     */
     onRemoveClick() {
       if (this.selectedNode) {
         this.removeElement(this.selectedNode);
-        this.isContextMenuVisible = false;
         this.selectedNode = null;
+      }
+
+      this.isContextMenuVisible = false;
+    },
+
+    onSetAsStartingClick() {
+      if (this.selectedNode) {
+        let currentStarting = this.cytoscape.elements("node[isFirst = true]");
+
+        currentStarting.forEach(element => {
+          this.setElementAsIsFirst(element, false);
+        })
+
+        this.setElementAsIsFirst(this.selectedNode, true);
+        this.selectedNode.unselect();
+      }
+
+      this.isContextMenuVisible = false;
+    },
+
+    setElementAsIsFirst(element, value) {
+      element.data("isFirst", value);
+      value ? element.addClass("first-lesson") : element.removeClass("first-lesson");
+    },
+
+    setContextMenuVisibility() {
+      this.contextMenuVisibility = {
+        remove: true,
+        setAsStarting: !(this.selectedNode && this.selectedNode.data("isFirst"))
       }
     }
   }
